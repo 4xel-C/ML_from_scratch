cde5c6c01f330bef75f75c439bec17fa9afda692
Axel Cano <axel-cano@live.fr>
2026-09-05 21:52:16 +0200

    Constructor method + distances calculations using broadcasting

--- dimensionality_reduction/umap.py
+++ dimensionality_reduction/umap.py
@@ -8,6 +8,11 @@ How to compute UMAP:
 3) Optimize using the binary cross-entropy to rebuild the graph
 """
 
+from typing import List, Tuple
+
+import numpy as np
+from numpy.typing import NDArray
+
 
 class UMAP:
     def __init__(
@@ -17,4 +22,21 @@ class UMAP:
         learning_rate: float,
         n_epochs: int,
         n_negative_sample: int,
-    ): ...
+        min_dist: float,
+    ):
+        self.n_neighbors = n_neighbors
+        self.n_components = n_components
+        self.learning_rate = learning_rate
+        self.n_epochs = n_epochs
+        self.n_negative_sample = n_negative_sample
+        self.min_dist = min_dist
+
+    def fit_transform(self, X: NDArray) -> NDArray:
+        # =============== KNN computing
+
+        # initialize the variables to store the indices of the knn and the distances
+        knn_indices: NDArray = np.full((len(X), self.n_neighbors), -1)
+        knn_distances: NDArray = np.full((len(X), self.n_neighbors), -1)
+
+        # compute the distances using broadcasting
+        distances = np.sum((X[np.newaxis, :, :] - X[:, np.newaxis, :]) ** 2, axis=2)
